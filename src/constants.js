4aaf8bfb50ad73230c8cc6ac118bf641bce24c17
Daniel Justin <danieljmuleke@gmail.com>
2026-06-29 05:49:31 +0200

    fix: correct rights constants for ETL module

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,8 @@
 export const MSR_ETL_MODULE_NAME = 'msrEtl';
 
 // Rights
-export const RIGHT_MSR_ETL_SEARCH = 190001;
+export const RIGHT_MSR_ETL_SEARCH = 953001;
+export const RIGHT_MSR_ETL_EXPORT = 953002;
 
 // ETL service class names (must match the Python class names on the backend)
 export const MSR_ETL_SERVICES = {
