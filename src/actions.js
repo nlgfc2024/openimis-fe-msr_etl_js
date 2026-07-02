c47c47242f86cfe7d221aa88210b9fcadd7d5e04
Daniel Justin <danieljmuleke@gmail.com>
2026-07-02 08:51:40 +0200

    feat: implement execute ETL service functionality and update related state management

--- src/actions.js
+++ src/actions.js
@@ -2,6 +2,7 @@
 /* eslint-disable camelcase */
 import {
   graphql,
+  graphqlMutation,
   formatQuery,
 } from '@openimis/fe-core';
 import { ACTION_TYPE } from './reducer';
@@ -30,21 +31,29 @@ export function fetchMsrEtlServices() {
   return graphql(payload, ACTION_TYPE.FETCH_ETL_SERVICES);
 }
 
-export function fetchMsrUbrIndividuals(filters = {}) {
-  const projection = [
-    'count',
-  ];
-  const params = {};
-  if (filters.location?.district) params.district = filters.location.district;
-  if (filters.location?.ta) params.ta = filters.location.ta;
-  if (filters.location?.village) params.village = filters.location.village;
-  if (filters.classification) params.classification = filters.classification;
-  if (filters.percentile) params.percentile = filters.percentile;
-  if (filters.minAge) params.minAge = filters.minAge;
-  if (filters.maxAge) params.maxAge = filters.maxAge;
-  if (filters.gender) params.gender = filters.gender;
-  const payload = formatQuery('msrUbrIndividuals', params, projection);
-  return graphql(payload, ACTION_TYPE.FETCH_UBR_INDIVIDUALS);
+const EXECUTE_ETL_SERVICE_MUTATION = `
+  mutation executeMsrEtlService($input: MsrEtlServiceMutationInput!) {
+    executeMsrEtlService(input: $input) {
+      clientMutationId
+      internalId
+    }
+  }
+`;
+
+export function executeMsrEtlService(serviceName, filters = {}) {
+  const input = { nameOfService: serviceName };
+  if (filters.location?.district) input.district = filters.location.district;
+  if (filters.location?.ta) input.ta = filters.location.ta;
+  if (filters.location?.village) input.village = filters.location.village;
+  if (filters.classifications?.length) input.classification = filters.classifications.map((c) => c.value);
+  if (filters.percentile != null) {
+    input.lowerPercentileCategory = 0;
+    input.upperPercentileCategory = filters.percentile;
+  }
+  if (filters.minAge != null) input.minAge = filters.minAge;
+  if (filters.maxAge != null) input.maxAge = filters.maxAge;
+  if (filters.gender) input.gender = filters.gender;
+  return graphqlMutation(EXECUTE_ETL_SERVICE_MUTATION, { input }, ACTION_TYPE.EXECUTE_ETL_SERVICE);
 }
 
 // ---------------------
@@ -55,6 +64,6 @@ export const clearEtlServices = () => (dispatch) => {
   dispatch({ type: CLEAR(ACTION_TYPE.FETCH_ETL_SERVICES) });
 };
 
-export const clearMsrUbrIndividuals = () => (dispatch) => {
-  dispatch({ type: CLEAR(ACTION_TYPE.FETCH_UBR_INDIVIDUALS) });
+export const clearMsrEtlExecution = () => (dispatch) => {
+  dispatch({ type: CLEAR(ACTION_TYPE.EXECUTE_ETL_SERVICE) });
 };
